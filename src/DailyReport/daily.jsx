a1e7bc36c456071e371fd860b0195b365d160e9d
Noah Hoffman <stacknoah99@gmail.com>
2023-06-27 16:42:54 -0600

    styling overall layout

--- src/DailyReport/daily.jsx
+++ src/DailyReport/daily.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-
+import './index.css'
 
 function DailyReport({ searchedCity }) {
   useEffect(() => {
@@ -7,9 +7,9 @@ function DailyReport({ searchedCity }) {
   }, [searchedCity]);
 
   const fetchData = (searchedCity) => {
-    // const url = `https://open-weather13.p.rapidapi.com/city/${searchedCity}`;
-    const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=5`;
     
+    // const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=5`;
+    const url = `https://weatherapi-com.p.rapid`
 
     const options = {
         method: 'GET',
@@ -42,9 +42,49 @@ function DailyReport({ searchedCity }) {
 
 
   return (
-    <div className="container mx-auto w-100vw bg-white p-6 rounded-lg shadow-lg">
-      
-    </div>
+    <main className="container mx-auto w-100vw bg-white p-6 rounded-lg shadow-lg">
+      <div className="locationContainer">
+        <p>Colorado</p>
+        <p>Denver</p>
+        <p>Monday</p>
+        <p>9:00 AM</p>
+      </div>
+      <div className="dailyContainer">
+          <div>Weather ICON Here</div>
+          <div>
+              <p>61</p>
+              <p>74</p>
+              <p>76</p>
+          </div>
+      </div>
+      <div className="forecastContainer">
+          <div id="day1">
+              <p>Monday</p>
+              <p>72</p>
+              <p>54</p>
+          </div>
+          <div id="day2">
+          <p>Tuesday</p>
+              <p>72</p>
+              <p>54</p>
+          </div>
+          <div id="day3">
+          <p>Wednsday</p>
+              <p>72</p>
+              <p>54</p>
+          </div>
+          <div id="day4">
+          <p>Thursday</p>
+              <p>72</p>
+              <p>54</p>
+          </div>
+          <div id="day5">
+          <p>Friday</p>
+              <p>72</p>
+              <p>54</p>
+          </div>
+      </div>
+    </main>
   );
 }
 export { DailyReport };
