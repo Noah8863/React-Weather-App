ed2b6c0e333e3d3a4dfac43e9839addf4a76956e
Noah Hoffman <stacknoah99@gmail.com>
2023-06-27 17:06:01 -0600

    more overall styles completed

--- src/DailyReport/daily.jsx
+++ src/DailyReport/daily.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-import './index.css'
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import "./index.css";
 
 function DailyReport({ searchedCity }) {
   useEffect(() => {
@@ -7,16 +9,15 @@ function DailyReport({ searchedCity }) {
   }, [searchedCity]);
 
   const fetchData = (searchedCity) => {
-    
-    // const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=5`;
-    const url = `https://weatherapi-com.p.rapid`
+    const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=5`;
+    // const url = `https://weatherapi-com.p.rapid`;
 
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '5ead5cf470msh36ab28d485526e2p1a0624jsn33e9f2c7cbfd',
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-        }
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "5ead5cf470msh36ab28d485526e2p1a0624jsn33e9f2c7cbfd",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
     };
 
     fetch(url, options)
@@ -30,7 +31,6 @@ function DailyReport({ searchedCity }) {
         if (data) {
           // Process the fetched data
           console.log(data);
-          
         }
       })
       .catch((error) => {
@@ -40,49 +40,83 @@ function DailyReport({ searchedCity }) {
       });
   };
 
-
   return (
     <main className="container mx-auto w-100vw bg-white p-6 rounded-lg shadow-lg">
       <div className="locationContainer">
-        <p>Colorado</p>
         <p>Denver</p>
-        <p>Monday</p>
-        <p>9:00 AM</p>
+        <p style={{ fontSize: 14 }}>Colorado</p>
+        <p>Monday 9:00 AM</p>
       </div>
       <div className="dailyContainer">
-          <div>Weather ICON Here</div>
+        <div>Weather ICON Here</div>
+        <div id="currentTemps">
           <div>
-              <p>61</p>
-              <p>74</p>
-              <p>76</p>
+            <KeyboardArrowUpIcon />
+            76
           </div>
+          <p style={{ fontSize: 28 }}>74</p>
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
       </div>
       <div className="forecastContainer">
-          <div id="day1">
-              <p>Monday</p>
-              <p>72</p>
-              <p>54</p>
+        <div id="day1">
+          <p>Monday</p>
+          <div>
+            <KeyboardArrowUpIcon />
+            76
           </div>
-          <div id="day2">
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
+        <div id="day2">
           <p>Tuesday</p>
-              <p>72</p>
-              <p>54</p>
+          <div>
+            <KeyboardArrowUpIcon />
+            76
           </div>
-          <div id="day3">
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
+        <div id="day3">
           <p>Wednsday</p>
-              <p>72</p>
-              <p>54</p>
+          <div>
+            <KeyboardArrowUpIcon />
+            76
           </div>
-          <div id="day4">
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
+        <div id="day4">
           <p>Thursday</p>
-              <p>72</p>
-              <p>54</p>
+          <div>
+            <KeyboardArrowUpIcon />
+            76
           </div>
-          <div id="day5">
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
+        <div id="day5">
           <p>Friday</p>
-              <p>72</p>
-              <p>54</p>
+          <div>
+            <KeyboardArrowUpIcon />
+            76
           </div>
+          <div>
+            <KeyboardArrowDownIcon />
+            61
+          </div>
+        </div>
       </div>
     </main>
   );
