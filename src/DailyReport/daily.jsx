55d0cfec01ccabfd9bc8ce4d51e91b369b5b6fb7
Noah Hoffman <stacknoah99@gmail.com>
2023-06-27 17:49:25 -0600

    starting to inject data into the HTML

--- src/DailyReport/daily.jsx
+++ src/DailyReport/daily.jsx
@@ -31,6 +31,7 @@ function DailyReport({ searchedCity }) {
         if (data) {
           // Process the fetched data
           console.log(data);
+          updateHTML(data);
         }
       })
       .catch((error) => {
@@ -40,85 +41,126 @@ function DailyReport({ searchedCity }) {
       });
   };
 
+  function updateHTML(data) {
+    //Current Searched City Data
+    const cityName = document.getElementById("cityName");
+    const cityState = document.getElementById("cityState");
+    const localTime = document.getElementById("localTime");
+
+    //Current Temp from search
+    // const weatherIcon = document.getElementById("localTime");
+    const windSpeed = document.getElementById("localTime");
+    const UVIndex = document.getElementById("localTime");
+
+    //5 Day Forecast Variables
+
+    //Convert time from Military time
+    const localtime = (localTime.textContent = data.location.localtime);
+    const timeParts = localtime.split(" ")[1].split(":");
+    // Split the time at ":" into hours and minutes
+
+    let hours = parseInt(timeParts[0]);
+    const minutes = timeParts[1];
+
+    // Convert the hours to 12-hour format
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12; // If hours is 0, set it to 12
+
+    const formattedTime = `${hours}:${minutes} ${meridiem}`;
+
+    console.log(formattedTime); // Output: 5:10 PM
+
+    // Update the HTML content with the fetched data
+    cityName.textContent = data.location.name;
+    cityState.textContent = data.location.region;
+    localTime.textContent = `Local Time: ${formattedTime}`;
+
+    // const weatherIcon = data.current.condition.icon
+  }
+
   return (
-    <main className="container mx-auto w-100vw bg-white p-6 rounded-lg shadow-lg">
-      <div className="locationContainer">
-        <p>Denver</p>
-        <p style={{ fontSize: 14 }}>Colorado</p>
-        <p>Monday 9:00 AM</p>
-      </div>
-      <div className="dailyContainer">
-        <div>Weather ICON Here</div>
-        <div id="currentTemps">
-          <div>
-            <KeyboardArrowUpIcon />
-            76
-          </div>
-          <p style={{ fontSize: 28 }}>74</p>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
+    <div>
+      <main className="container mx-auto w-100vw bg-white p-6 rounded-lg shadow-lg">
+        <div className="locationContainer">
+          <p id="cityName" style={{ fontSize: 26 }}>
+            Denver
+          </p>
+          <p id="cityState">Colorado</p>
+          <p id="localTime">Monday 9:00 AM</p>
+        </div>
+
+        <div className="dailyContainer">
+          <div id="currentTemps">
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <p style={{ fontSize: 28 }}>74</p>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
           </div>
         </div>
-      </div>
-      <div className="forecastContainer">
-        <div id="day1">
-          <p>Monday</p>
-          <div>
-            <KeyboardArrowUpIcon />
-            76
+        <div className="forecastContainer">
+          <div id="day1">
+            <p>Monday</p>
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
           </div>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
+          <div id="day2">
+            <p>Tuesday</p>
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
+          </div>
+          <div id="day3">
+            <p>Wednsday</p>
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
+          </div>
+          <div id="day4">
+            <p>Thursday</p>
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
+          </div>
+          <div id="day5">
+            <p>Friday</p>
+            <div>
+              <KeyboardArrowUpIcon />
+              76
+            </div>
+            <div>
+              <KeyboardArrowDownIcon />
+              61
+            </div>
           </div>
         </div>
-        <div id="day2">
-          <p>Tuesday</p>
-          <div>
-            <KeyboardArrowUpIcon />
-            76
-          </div>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
-          </div>
-        </div>
-        <div id="day3">
-          <p>Wednsday</p>
-          <div>
-            <KeyboardArrowUpIcon />
-            76
-          </div>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
-          </div>
-        </div>
-        <div id="day4">
-          <p>Thursday</p>
-          <div>
-            <KeyboardArrowUpIcon />
-            76
-          </div>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
-          </div>
-        </div>
-        <div id="day5">
-          <p>Friday</p>
-          <div>
-            <KeyboardArrowUpIcon />
-            76
-          </div>
-          <div>
-            <KeyboardArrowDownIcon />
-            61
-          </div>
-        </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
 export { DailyReport };
