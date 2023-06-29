593da11e6d7a611cbc2c43bb66541e1bda038605
Noah8863 <stacknoah99@gmail.com>
2023-06-28 18:10:20 -0600

    added TODO list and forecast containers

--- src/DailyReport/daily.jsx
+++ src/DailyReport/daily.jsx
@@ -17,7 +17,13 @@ function DailyReport({ searchedCity }) {
   const [day1, setDay1] = useState("Monday");
   const [day2, setDay2] = useState("Tuesday");
   const [day3, setDay3] = useState("Wednesday");
-  const [day1High, setDay1High] = useState("Monday");
+  const [day2Icon, setDay2Icon] = useState(cloudyIcon)
+  const [day3Icon, setDay3Icon] = useState(rainyIcon)
+  const [day1Description, setday1Description] = useState ("Sunny")
+  const [day2Description, setday2Description] = useState ("Cloudy")
+  const [day3Description, setday3Description] = useState ("Rainy")
+
+  const [day1High, setDay1High] = useState("");
   const [day1Low, setDay1Low] = useState("");
   const [day2High, setDay2High] = useState("");
   const [day2Low, setDay2Low] = useState("");
@@ -45,10 +51,13 @@ function DailyReport({ searchedCity }) {
         }
         return response.json();
       })
+      // TODO Need to figure out conflicting weather reports 
+      // Current Weather does not match up with overall daily weather condintion
+      // Could make a 4th switch case JUST for the overall weather... uuuugh. sloppy code
       .then((data) => {
         if (data) {
           console.log(data);
-          var weatherIcon = data.current.condition.text.toLowerCase();
+          var weatherIcon = data.forecast.forecastday[0].day.condition.text.toLowerCase();
           switch (weatherIcon) {
             case "sunny":
               setIcon(sunnyIcon);
@@ -68,17 +77,100 @@ function DailyReport({ searchedCity }) {
             case "storm":
               setIcon(stormIcon);
               break;
+            case "overcast":
+              setIcon(cloudyIcon);
+              break;
+            case "patchy rain possible":
+              setIcon(cloudyIcon);
+              break;
             default:
               setIcon(null);
               break;
           }
-          setDay1(data.forecast.forecastday[0].date.split("-").slice(1).join("-"));
-          setDay2(data.forecast.forecastday[1].date.split("-").slice(1).join("-"));
-          setDay3(data.forecast.forecastday[2].date.split("-").slice(1).join("-"));
+          var secondWeatherIcon = data.forecast.forecastday[1].day.condition.text.toLowerCase();
+          switch (secondWeatherIcon) {
+            case "sunny":
+              setDay2Icon(sunnyIcon);
+              break;
+            case "clear":
+              setDay2Icon(sunnyIcon);
+              break;
+            case "patchy rain possible":
+              setDay2Icon(partlyCloudyIcon);
+              break;
+            case "cloudy":
+              setDay2Icon(cloudyIcon);
+              break;
+            case "partly cloudy":
+              setDay2Icon(partlyCloudyIcon);
+              break
+            case "moderate rain":
+              setDay2Icon(rainyIcon);
+              break;
+            case "storm":
+              setDay2Icon(stormIcon);
+              break;
+            default:
+              setDay2Icon(null);
+              break;
+          }
+          var thirdWeatherIcon = data.forecast.forecastday[2].day.condition.text.toLowerCase();
+          switch (thirdWeatherIcon) {
+            case "sunny":
+              setDay3Icon(sunnyIcon);
+              break;
+            case "clear":
+              setDay3Icon(sunnyIcon);
+              break;
+            case "patchy rain possible":
+              setDay3Icon(partlyCloudyIcon);
+              break;
+            case "cloudy":
+              setDay3Icon(cloudyIcon);
+              break;
+            case "partly cloudy":
+              setDay3Icon(partlyCloudyIcon);
+              break
+            case "moderate rain":
+              setDay3Icon(rainyIcon);
+              break;
+            case "storm":
+              setDay3Icon(stormIcon);
+              break;
+            default:
+              setDay3Icon(null);
+              break;
+          }
+          const [year1, month1, day1] = data.forecast.forecastday[0].date.split("-");
+          const firstDateObj = new Date(year1, month1 - 1, day1);
+          const firstMonthName = firstDateObj.toLocaleString("en-US", { month: "long" });
+          const firstFormattedDay = day1.padStart(2, "0");
+          const firstFormattedDate = `${firstMonthName} ${firstFormattedDay}`;
+          setDay1(firstFormattedDate);
+
+          const [year2, month2, day2] = data.forecast.forecastday[1].date.split("-");
+          const secondDateObj = new Date(year2, month2 - 1, day2);
+          const secondMonthName = secondDateObj.toLocaleString("en-US", { month: "long" });
+          const secondFormattedDay = day2.padStart(2, "0");
+          const secondFormattedDate = `${secondMonthName} ${secondFormattedDay}`;
+          setDay2(secondFormattedDate);
+
+          const [year3, month3, day3] = data.forecast.forecastday[2].date.split("-");
+          const thirdDateObj = new Date(year3, month3 - 1, day3);
+          const thirdMonthName = thirdDateObj.toLocaleString("en-US", { month: "long" });
+          const thirdFormattedDay = day2.padStart(2, "0");
+          const thirdFormattedDate = `${thirdMonthName} ${thirdFormattedDay}`;
+          setDay3(thirdFormattedDate);
+
           setDay1High(data.forecast.forecastday[1].day.maxtemp_f);
           setDay1Low(data.forecast.forecastday[1].day.mintemp_f);
           setDay2High(data.forecast.forecastday[2].day.maxtemp_f);
           setDay2Low(data.forecast.forecastday[2].day.mintemp_f);
+
+          setday1Description(data.forecast.forecastday[0].day.condition.text)
+          setday2Description(data.forecast.forecastday[1].day.condition.text)
+          setday3Description(data.forecast.forecastday[2].day.condition.text)
+
           updateHTML(data);
         }
       })
@@ -116,8 +208,8 @@ function DailyReport({ searchedCity }) {
     <div className="flex h-screen p-2">
       <div className="w-1/3 h-full bg-gray-600 opacity-70 text-center">
         <div className="h-3/5 ">
-          <p className="text-2xl p-4">Location</p>
-          <p id="cityName" className="text-2xl p-2">
+          <p className="text-xl p-4">Location</p>
+          <p id="cityName" className="text-xxl p-2">
             {cityName}
           </p>
           <p id="currentTemp" className="text-3xl p-8">
@@ -126,14 +218,26 @@ function DailyReport({ searchedCity }) {
           <div className="flex justify-center my-8">
             {icon && <img src={icon} alt="Weather Icon" className="w-1/4" />}
           </div>
-          <p id="weatherCondition" className="text-lg">
+          <p id="weatherCondition" className="text-xxl">
             {weatherCondition}
           </p>
         </div>
         <div className="h-2/5 flex">
-          <div className="flex-grow bg-red-400 text-xxl pt-8">{day1}</div>
-          <div className="flex-grow bg-green-400 text-xxl pt-8">{day2}</div>
-          <div className="flex-grow bg-pink-400 text-xxl pt-8">{day3}</div>
+          <div className="flex-grow bg-red-400 text-xxl pt-8">
+            <p>{day1}</p>
+            {icon && <img src={icon} alt="Weather Icon" className="h-16 w-16 m-auto mt-4" />}
+            <p className="text-xl pt-8">{day1Description}</p>
+            </div>
+          <div className="flex-grow bg-green-400 text-xxl pt-8">
+          <p>{day2}</p>
+            {day2Icon && <img src={day2Icon} alt="Weather Icon" className="h-16 w-16 m-auto mt-4" />}
+            <p className="text-xl pt-8">{day2Description}</p>
+          </div>
+          <div className="flex-grow bg-pink-400 text-xxl pt-8">
+          <p>{day3}</p>
+            {day3Icon && <img src={day3Icon} alt="Weather Icon" className="h-16 w-16 m-auto mt-4" />}
+            <p className="text-xl pt-8">{day3Description}</p>
+          </div>
         </div>
       </div>
       <div className="w-2/5 h-full bg-gray-600 opacity-70">
